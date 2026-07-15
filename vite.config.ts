56a05b4a9efa118d6c890088841d93ddbb572ba6
Stephen <stephen.isienyi@webkrafting.com>
2026-07-14 19:37:04 -0700

    addressing vite/svelte build/publish concerns

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,12 @@
-import { defineConfig } from 'vitest/config';
-import { playwright } from '@vitest/browser-playwright';
 import { sveltekit } from '@sveltejs/kit/vite';
-import adapter from '@sveltejs/adapter-node';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-
+import { playwright } from '@vitest/browser-playwright';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [
-		sveltekit({
-			compilerOptions: {
-				// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
-				runes: ({ filename }) =>
-					filename.split(/[/\\]/).includes('node_modules') ? undefined : true
-			},
-			preprocess: vitePreprocess(),
-			adapter: adapter()
-		})
-	],
+	plugins: [ sveltekit() ],
 	test: {
 		coverage: {
 			include: [
-				'src/lib/index.{svelte}.{test,spec}.{js,ts}',
-				'src/lib/main/**/*.svelte.{test,spec}.{js,ts}',
-
 				'src/**/*.{ts,tsx,js,jsx}'
 			],
 			exclude: [
@@ -32,34 +16,34 @@ export default defineConfig({
 				'src/lib/vitest-examples'
 			],
 			provider: 'v8', // or 'istanbul'
-			reporter: ['text', 'json', 'html', 'lcov'],
+			reporter: [ 'text', 'json', 'html', 'lcov' ],
 			reportOnFailure: true, 
 			reportsDirectory: './coverage' // Adjust to your source directory
 		},
-		expect: { requireAssertions: true },
-		projects: [
-			{
-				extends: './vite.config.ts',
-				test: {
-					name: 'client',
-					browser: {
-						enabled: true,
-						provider: playwright(),
-						instances: [{ browser: 'chromium', headless: true }]
-					},
-					include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
-					exclude: ['src/lib/server/**']
-				}
-			},
-			{
-				extends: './vite.config.ts',
-				test: {
-					name: 'server',
-					environment: 'node',
-					include: ['src/**/*.{test,spec}.{js,ts}'],
-					exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
-				}
+		include: [ 'src/**/*.{test,spec}.{js,ts}' ],
+		projects: [{
+			extends: './vite.config.ts',
+			test: {
+				name: 'client',
+				browser: {
+					enabled: true,
+					provider: playwright(),
+					instances: [{
+						browser: 'chromium',
+						headless: true
+					}]
+				},
+				include: [ 'src/**/*.svelte.{test,spec}.{js,ts}' ],
+				exclude: [ 'src/lib/server/**' ]
 			}
-		]
+		}, {
+			extends: './vite.config.ts',
+			test: {
+				name: 'server',
+				environment: 'node',
+				include: [ 'src/**/*.{test,spec}.{js,ts}' ],
+				exclude: [ 'src/**/*.svelte.{test,spec}.{js,ts}' ]
+			}
+		}]
 	}
 });
